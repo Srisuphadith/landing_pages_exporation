c1ed3359feb4c908ffbbdb1716e36e3b5d3d6878
Srisuphadith <75804199+Srisuphadith@users.noreply.github.com>
2023-11-04 12:15:25 +0700

    v.6

---
+++ javas.js
@@ -0,0 +1,15 @@
+function scroll1() {
+    window.scrollTo(0, 0);
+}
+function scroll2() {
+    window.scrollTo(0, 750);
+}
+function scroll3() {
+    window.scrollTo(0, 1450);
+}
+function scroll4() {
+    window.scrollTo(0, 2300);
+}
+function scroll5() {
+    window.scrollTo(0, 2500);
+}
